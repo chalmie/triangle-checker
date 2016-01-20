8dd1db97fc48c6bb4424699be446754775f252c7
blair peterson and rob reasoner <reasoner55@hotmail.com>
2016-01-19 16:04:24 -0800

    finish specs.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,30 @@
 var checker = function(side1,side2,side3) {
-if((side1 === side2) && (side2 === side3)) {
-  return true;
-}
+  if ((side1 === side2) && (side2 === side3)) {
+    return "equilateral";
+  } else if ((side1 + side2 <= side3) || (side1 + side3 <= side2) || (side2 + side3 <= side1))
+  {
+    return "not a triangle";
+  }
+  else if (((side1 === side2) && (side1 !== side3)) || ((side2 === side3) && (side1 !== side3)) || ((side1 === side3) && (side2 !== side3)))
+  {
+    return "isosceles";
+  }
   else {
-    return false;
+    return "scalene";
   }
 };
+
+
+
+$(document).ready(function () {
+  $("form#checking").submit(function(event) {
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+    var result = checker(side1, side2, side3);
+
+    $(".checker").text(result);
+    $("#result").show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,15 @@
-describe("checker", function() {
+describe('checker', function() {
   it("is equilateral for all sides equal", function() {
-    expect(checker(4,4,4)).to.equal(true);
+    expect(checker(4,4,4)).to.equal('equilateral');
+  });
+
+  it("is not a triangle", function() {
+    expect(checker(1,1,4)).to.equal("not a triangle");
+  });
+  it("is a isosceles triangle", function() {
+    expect(checker(3,3,5)).to.equal("isosceles");
+  });
+  it("is a scalene triangle", function() {
+    expect(checker(3,4,5)).to.equal("scalene");
   });
 });
